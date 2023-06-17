e9e6eeeeaf39a3b92668089624d11b0f835fb186
Luis Vazquez <lvcarmona1907@hotmail.com>
2023-06-18 00:22:56 +0200

    primer commit

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,28 @@
+import { Character } from './../../interfaces/character.interface';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] =[{
+    name: 'Trunks',
+    power: 10
+  }];
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?: string):void{
+    // TODO: Emitir el ID del personaje
+
+    if(!id) return;
+
+    this.onDelete.emit(id);
+  }
+
+}
